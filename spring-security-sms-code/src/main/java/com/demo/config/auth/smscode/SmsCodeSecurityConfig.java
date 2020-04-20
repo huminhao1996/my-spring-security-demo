560986c40ac3cdf3493c62497d540f6d3ed4293c
hmh <454977355@qq.com>
2020-04-20 14:30:10 +0800

    短信验证的注释

--- spring-security-sms-code/src/main/java/com/demo/config/auth/smscode/SmsCodeSecurityConfig.java
+++ spring-security-sms-code/src/main/java/com/demo/config/auth/smscode/SmsCodeSecurityConfig.java
@@ -37,11 +37,13 @@ public class SmsCodeSecurityConfig extends SecurityConfigurerAdapter<DefaultSecu
         smsCodeAuthenticationFilter.setAuthenticationSuccessHandler(myAuthenticationSuccessHandler);
         smsCodeAuthenticationFilter.setAuthenticationFailureHandler(myAuthenticationFailureHandler);
 
+        // 获取验证码提供者
         SmsCodeAuthenticationProvider smsCodeAuthenticationProvider = new SmsCodeAuthenticationProvider();
         smsCodeAuthenticationProvider.setUserDetailsService(myUserDetailsService);
 
-        //过滤器的执行顺序
+        //在用户密码过滤器前面加入短信验证码校验过滤器
         http.addFilterBefore(smsCodeValidateFilter,UsernamePasswordAuthenticationFilter.class);
+        //在用户密码过滤器后面加入短信验证码认证授权过滤器
         http.authenticationProvider(smsCodeAuthenticationProvider)
                 .addFilterAfter(smsCodeAuthenticationFilter,UsernamePasswordAuthenticationFilter.class);
     }
